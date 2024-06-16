3070b11538b8d5b470994e1720df0a77e452c385
thibault alexandre <thibault.a@hotmail.com>
2024-06-16 18:28:31 +0200

    adding a link in the logging button

--- airbnb_django/urls.py
+++ airbnb_django/urls.py
@@ -27,7 +27,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('airbnb.urls')),
-    path('login/', login_view, name='loging'),
+    path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
 ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) ## Seems a bit ugly IMO, would need to ask for a refacto. Works without having to use it
