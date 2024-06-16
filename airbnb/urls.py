fb9791f7e83e43abd80b33232ad916c0307f6ba5
thibault alexandre <thibault.a@hotmail.com>
2024-06-16 19:04:10 +0200

    added a profile page and some links

--- airbnb/urls.py
+++ airbnb/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='home'),
+    path('profile/', views.profile_view, name='profile'),
 ]
 
--- airbnb/views.py
+++ airbnb/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 from .models import Flat
 
 # Create your views here.
@@ -10,4 +12,12 @@ def index(request):
         'flats': Flat.objects.all()
     }
     return render(request, 'airbnb/index.html', context)
+
+@login_required
+def profile_view(request):
+    user_flats = Flat.objects.filter(owner = request.user.userprofile)
+    context={
+        'user_flats':user_flats
+    }
+    return render(request, 'airbnb/profile.html', context)
 
